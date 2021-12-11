51501414680efd338959057fd85d05e8e0f4d9a8
PleaseDo <PleaseDo01@hotmail.com>
2021-12-11 00:45:29 -0600

    Add config db

--- config/db.config.js
+++ config/db.config.js
@@ -4,11 +4,12 @@ const mysql = require("mysql");
 
 //local mysql db connection
 const dbConn = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "innodb",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "b7153bb8833ae0",
+	password: "b015ac20",
+	database: "heroku_7dbd05eb640091d",
 });
+
 dbConn.connect(function (err) {
 	if (err) throw err;
 	console.log("Database Connected!");
